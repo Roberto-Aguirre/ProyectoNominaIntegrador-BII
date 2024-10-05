28cd4180c1e10e11793cecc8b956a96455884b1b
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-10-05 11:09:51 -0600

    Initial Commit master branch

---
+++ ProyectoNominaINTBII/ProyectoNominaINTBII/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace ProyectoNominaINTBII.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
