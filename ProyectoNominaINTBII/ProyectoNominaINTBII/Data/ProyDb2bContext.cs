e6e2c89ce770bcb345be2a7fcf6f7db48caf5918
AldoMaresF <116591504+AldoMaresF@users.noreply.github.com>
2024-11-25 23:10:49 -0600

    xd

--- ProyectoNominaINTBII/ProyectoNominaINTBII/Data/ProyDb2bContext.cs
+++ ProyectoNominaINTBII/ProyectoNominaINTBII/Data/ProyDb2bContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Elfie.Serialization;
+using Microsoft.EntityFrameworkCore;
 using ProyectoNominaINTBII.Models;
 
 namespace ProyectoNominaINTBII.Data;
@@ -87,7 +88,7 @@ public partial class ProyDb2bContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-F3JTBSV;Initial Catalog=PROY_DB_2B;Integrated Security=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source = EliteBook\\SQLEXPRESS; Initial Catalog=PROY_DB_2B;Integrated Security=True;Trust Server Certificate=True");
 
 
         
--- ProyectoNominaINTBII/ProyectoNominaINTBII/obj/Debug/net8.0/ProyectoNominaINTBII.AssemblyInfo.cs
+++ ProyectoNominaINTBII/ProyectoNominaINTBII/obj/Debug/net8.0/ProyectoNominaINTBII.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoNominaINTBII")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccae856b8fedad694cdac5153c7569ddd8b786c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoNominaINTBII")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoNominaINTBII")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
